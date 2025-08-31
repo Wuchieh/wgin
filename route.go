d1507b4c74e3f75f6d836d6bcb860ed869c191df
Wuchieh <q20001116@gmail.com>
2025-08-31 14:58:29 +0800

    feat: add use method alias

--- route.go
+++ route.go
@@ -6,7 +6,15 @@ func UseGet[response any](router gin.IRoutes, path string, handler BaseHandler[r
 	router.GET(path, handler.GetHandlers()...)
 }
 
+func Get[response any](router gin.IRoutes, path string, handler BaseHandler[response]) {
+	router.GET(path, handler.GetHandlers()...)
+}
+
 func UseHandle[request, response any](router gin.IRoutes, method, path string, handler Handler[request, response]) {
+	router.Handle(method, path, handler.GetHandlers()...)
+}
+
+func Handle[request, response any](router gin.IRoutes, method, path string, handler Handler[request, response]) {
 	router.Handle(method, path, handler.GetHandlers()...)
 }
 
@@ -14,7 +22,15 @@ func UsePOST[request, response any](router gin.IRoutes, path string, handler Han
 	router.POST(path, handler.GetHandlers()...)
 }
 
+func POST[request, response any](router gin.IRoutes, path string, handler Handler[request, response]) {
+	router.POST(path, handler.GetHandlers()...)
+}
+
 func UseDELETE[request, response any](router gin.IRoutes, path string, handler Handler[request, response]) {
+	router.DELETE(path, handler.GetHandlers()...)
+}
+
+func DELETE[request, response any](router gin.IRoutes, path string, handler Handler[request, response]) {
 	router.DELETE(path, handler.GetHandlers()...)
 }
 
@@ -22,7 +38,15 @@ func UsePATCH[request, response any](router gin.IRoutes, path string, handler Ha
 	router.PATCH(path, handler.GetHandlers()...)
 }
 
+func PATCH[request, response any](router gin.IRoutes, path string, handler Handler[request, response]) {
+	router.PATCH(path, handler.GetHandlers()...)
+}
+
 func UsePUT[request, response any](router gin.IRoutes, path string, handler Handler[request, response]) {
+	router.PUT(path, handler.GetHandlers()...)
+}
+
+func PUT[request, response any](router gin.IRoutes, path string, handler Handler[request, response]) {
 	router.PUT(path, handler.GetHandlers()...)
 }
 
@@ -30,10 +54,22 @@ func UseOPTIONS[request, response any](router gin.IRoutes, path string, handler 
 	router.OPTIONS(path, handler.GetHandlers()...)
 }
 
+func OPTIONS[request, response any](router gin.IRoutes, path string, handler Handler[request, response]) {
+	router.OPTIONS(path, handler.GetHandlers()...)
+}
+
 func UseHEAD[request, response any](router gin.IRoutes, path string, handler Handler[request, response]) {
 	router.HEAD(path, handler.GetHandlers()...)
 }
 
+func HEAD[request, response any](router gin.IRoutes, path string, handler Handler[request, response]) {
+	router.HEAD(path, handler.GetHandlers()...)
+}
+
 func UseAny[request, response any](router gin.IRoutes, path string, handler Handler[request, response]) {
+	router.Any(path, handler.GetHandlers()...)
+}
+
+func Any[request, response any](router gin.IRoutes, path string, handler Handler[request, response]) {
 	router.Any(path, handler.GetHandlers()...)
 }
